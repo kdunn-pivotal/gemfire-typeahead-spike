3ac9e5fc4e34b78da0838f57c6815c6dbdbb8ca9
Zac Hansen <zhansen@pivotal.io>
2017-03-28 17:01:55 -0600

    working lucene index demo for type ahead

--- src/main/java/io/pivotal/TypeaheadSpikeApplicationConfiguration.java
+++ src/main/java/io/pivotal/TypeaheadSpikeApplicationConfiguration.java
@@ -2,20 +2,18 @@ package io.pivotal;
 
 import java.util.Properties;
 
+import org.apache.geode.cache.*;
+import org.apache.geode.cache.lucene.LuceneService;
+import org.apache.geode.cache.lucene.LuceneServiceProvider;
+import org.apache.geode.cache.server.CacheServer;
+import org.apache.geode.pdx.ReflectionBasedAutoSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.gemfire.CacheFactoryBean;
-import org.springframework.data.gemfire.ReplicatedRegionFactoryBean;
-import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
-
-import com.gemstone.gemfire.cache.GemFireCache;
-import com.gemstone.gemfire.pdx.ReflectionBasedAutoSerializer;
 
 import io.pivotal.model.Property;
 
 @Configuration
-@EnableGemfireRepositories
 public class TypeaheadSpikeApplicationConfiguration {
 
     @Value("${embedded-locator-host:localhost}")
@@ -23,16 +21,16 @@ public class TypeaheadSpikeApplicationConfiguration {
 
     @Value("${embedded-locator-port:10334}")
     private int embeddedLocatorPort;
-    
+
     @Value("${cache-server.port:40404}")
     private int port;
-    
-    
+
+
     @Bean
     Properties geodeProperties() {
         Properties geodeProperties = new Properties();
         geodeProperties.setProperty("name", "DataGeodeTypeaheadSpike");
-        geodeProperties.setProperty("start-locator", embeddedLocatorHost + "[" + embeddedLocatorPort  + "]");
+        geodeProperties.setProperty("start-locator", embeddedLocatorHost + "[" + embeddedLocatorPort + "]");
         //geodeProperties.setProperty("start-dev-rest-api", "true");
         //geodeProperties.setProperty("http-service-port", "7070");
         geodeProperties.setProperty("jmx-manager", "true");
@@ -40,46 +38,33 @@ public class TypeaheadSpikeApplicationConfiguration {
         return geodeProperties;
     }
 
-    @Bean
-    CacheFactoryBean geodeCache() {
-        CacheFactoryBean geodeCache = new CacheFactoryBean();
-        geodeCache.setClose(true);
-        //geodeCache.setPdxReadSerialized(true);
-        //geodeCache.setPdxPersistent(true);
-        geodeCache.setPdxSerializer(new ReflectionBasedAutoSerializer("io.pivotal.model"));
-        geodeCache.setProperties(geodeProperties());
-        return geodeCache;
-    }
-    
-    /* Use the following if not using SDG
+
+    // Use the following if not using SDG
     @Bean
     public Cache createCache() throws Exception {
-    	
-        //LOG.info("creating Cache: locators={}, port={}", locators, port);
         CacheFactory cf = new CacheFactory();
-        cf.set("start-locator", embeddedLocatorHost + "[" + embeddedLocatorPort  + "]");
-
+        cf.set("start-locator", embeddedLocatorHost + "[" + embeddedLocatorPort + "]");
         cf.setPdxPersistent(true);
         cf.setPdxSerializer(new ReflectionBasedAutoSerializer("io.pivotal.model.*"));
-
         Cache c = cf.create();
-
-        //LOG.info("Creating CacheServer");
         CacheServer cs = c.addCacheServer();
         cs.setPort(port);
         cs.start();
-
         return c;
     }
-    */
 
     @Bean
-    public ReplicatedRegionFactoryBean<Long, Property> propertyRegion(final GemFireCache cache) {   
-    	// RegionShortcut.REPLICATE
-    	ReplicatedRegionFactoryBean<Long, Property> rf = new ReplicatedRegionFactoryBean<>();
-    	rf.setCache(cache);
-    	rf.setName("Property");
-        return rf;
+    public LuceneService luceneService(Cache cache) {
+        return LuceneServiceProvider.get(cache);
     }
-    
+
+    @Bean
+    public Region<Integer, Property> propertyRegion(Cache cache, LuceneService luceneService) {
+        // Create Index on fields with default analyzer:
+        luceneService.createIndex("myIndex", "/Property", "Address");
+        Region region = cache.createRegionFactory(RegionShortcut.PARTITION).create("Property");
+        return region;
+    }
+
+
 }
--- src/main/java/io/pivotal/db/PropertyRepository.java
+++ src/main/java/io/pivotal/db/PropertyRepository.java
@@ -1,23 +1,11 @@
 package io.pivotal.db;
 
-import org.springframework.data.gemfire.mapping.Region;
-import org.springframework.data.gemfire.repository.Query;
-import org.springframework.data.gemfire.repository.query.annotation.Import;
-import org.springframework.data.repository.CrudRepository;
 
 import io.pivotal.model.Property;
 
-@Region("Property")
-public interface PropertyRepository extends CrudRepository<Property, Long> {
+public interface PropertyRepository {
 
-	@Query("SELECT * FROM /Property p WHERE p.address LIKE $1 LIMIT 5")
-	Iterable<Property> findAFewByAddressFuzzy(String address);
-	
-	Iterable<Property> findByAddressIgnoreCaseContaining(String address);
+    Iterable<Property> luceneFindAFewByAddressFuzzy(String address) throws Exception;
 
-	@Query("SELECT count(*) FROM /Property p")
-	Integer getCountOfProperties();
-
-	@Query("SELECT p.address FROM /Property p LIMIT 5")
-	Iterable<String> getProperties();
+    void save(Property property);
 }
